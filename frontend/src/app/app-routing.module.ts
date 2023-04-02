35274ffcccbe664f5b0a8945dec5b06ca0a619b9
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2023-04-02 13:02:03 +0200

    add routing

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './views/game/game.component';
+
+const routes: Routes = [
+  { path:'game', component: GameComponent },
+  { path:'', redirectTo:'/game', pathMatch:'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './views/game/game.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AnswerComponent } from './shared/answer/answer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    AnswerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
