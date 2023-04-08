747a31728976ccf313d88b65bc4a85f267963abb
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2023-04-08 02:41:23 +0200

    add generating calculations

--- frontend/src/app/shared/calculations/calculations.component.ts
+++ frontend/src/app/shared/calculations/calculations.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-calculations',
@@ -9,4 +9,35 @@ export class CalculationsComponent {
   @Input() a:number = 0;
   @Input() b:number = 0;
   @Input() symbol:String = '+';
+  @Output() answered = new EventEmitter<string>()
+  
+  checkAnswer(event:any){
+    if ((this.result())==event.target.value) {
+      this.answered.emit('true');
+      event.target.value = ""
+    }else if(event.key=="Enter"){
+      this.answered.emit('false');
+      event.target.value = ""
+    }
+  }
+
+  result():number{
+    switch(this.symbol){
+      case '*':{
+        return this.a*this.b;
+        break;
+      }
+      case '+':{
+        return this.a+this.b;
+      }
+      case ':':{
+        return this.a/this.b;
+      }
+      case '-':{
+        return this.a-this.b;
+      }
+    }
+    return 0;
+  }
+
 }
--- frontend/src/app/views/game/game.component.ts
+++ frontend/src/app/views/game/game.component.ts
@@ -6,4 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
+  numberA:number = this.generateNumber(1,10);
+  numberB:number = this.generateNumber(1,10);
+  symbol = this.generateSymbol();
+  answers:string[] = new Array(7);
+  index:number = 1;
+
+  ngOnInit(){
+    this.answers[0]="current";
+  }
+
+  generateNumber(min:number,max:number):number{
+    return  Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  generateSymbol():String{
+    let symbol = Math.floor(Math.random() * 4);
+    if(symbol == 1){
+      return "+";
+    }else if(symbol == 2){
+      return "-";
+    }else if(symbol == 3){
+      return "*";
+    }else{
+      return ":";
+    }
+  }
+
+  checkAnswer(event:any){
+    this.numberA = this.generateNumber(1,10);
+    this.numberB = this.generateNumber(1,10);
+    this.symbol = this.generateSymbol();
+    console.log(event)
+    if(event == 'true'){
+      this.answers[this.index-1] = "correct"  
+    }else{
+      this.answers[this.index-1] = "wrong"
+    }
+    this.answers[this.index] = "current"
+    this.index++
+    console.log(this.answers)
+  }
 }
